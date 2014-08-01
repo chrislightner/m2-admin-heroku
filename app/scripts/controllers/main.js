cc4e68d3730b21a37a83427e533aa4f4b8678dbd
Jason Fossella <jasonfossella@fossella.local>
2014-08-01 16:47:59 -0500

    changed controller so shortname is everything after the dash

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -134,7 +134,7 @@ angular.module('m2AdminApp')
 			campaign.jobNumber = $("#new-job-number").val();
 
 			// change the short name
-			campaign.shortName = $("#new-job-number").val().substr(5,8);
+			campaign.shortName = $("#new-job-number").val().substr($("#new-job-number").val().indexOf("-"),$("#new-job-number").val().length);
 
 			// create the campaign
 			$http.post('//coveragedetails.net/api/index.php/campaigns/new', campaign)
