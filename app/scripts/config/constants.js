694c68b1d3ce66e20277724ba92d4a8344746da3
Chris Lightner <chrislightner@Lightner-MacBook-Pro.local>
2019-12-09 20:26:42 -0600

    Updating api url

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -1,7 +1,7 @@
 angular.module('Constants', [])
 
   .constant('Constants', {
-    API_URL: 'https://m2-api-heroku.herokuapp.com/index.php/'
+    API_URL: 'https://api.insurancedetails.net/index.php/'
   })
 //.constant('Constants', {API_URL:'http://zeisgroupdevelopment.com/api/index.php/'})
 
