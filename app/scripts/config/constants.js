43692e217afaac25e479078ff087296da8913111
Chris Lightner <chrislightner@Lightner-MacBook-Pro.local>
2023-03-31 08:51:24 -0500

    Update API references

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -1,5 +1,10 @@
 angular.module('Constants', [])
 
-.constant('Constants', {API_URL:'http://zeisgroupdevelopment.com/api/index.php/'})
+  .constant('Constants', {
+    API_URL: 'https://m2-api-nest-test.herokuapp.com/'
+  })
+//.constant('Constants', {API_URL:'http://zeisgroupdevelopment.com/api/index.php/'})
+//https://m2-api-nest-test.herokuapp.com/
+//https://api.coveragedetails.net/index.php/
 
 ;
