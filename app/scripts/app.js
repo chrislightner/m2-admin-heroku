e2406462ab2c6c77c61ebd40ddc0fb6d97d7c7b1
Chris Lightner <chrislightner@Lightner-MacBook-Pro.local>
2023-04-03 15:41:01 -0500

    switch to new API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -125,8 +125,8 @@ angular.module('m2AdminApp', [
 
       if ($rootScope.currentUser.username) {
         //$http.post('//coveragedetails.net/api/index.php/users/check', $rootScope.currentUser)
-        $http.post('https://api.coveragedetails.net/index.php/users/check', $rootScope.currentUser)
-        //$http.post('https://m2-api-nest-test.herokuapp.com/users/check', $rootScope.currentUser)
+        //$http.post('https://api.coveragedetails.net/index.php/users/check', $rootScope.currentUser)
+        $http.post('https://m2-api-nest-test.herokuapp.com/users/check', $rootScope.currentUser)
           .success(function (data) {
             if (data.status !== "success") {
               if (data.error == 1) {
--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -1,8 +1,8 @@
 angular.module('Constants', [])
 
   .constant('Constants', {
-    API_URL: 'https://api.coveragedetails.net/index.php/'
-    //API_URL: 'https://m2-api-nest-test.herokuapp.com/'
+    //API_URL: 'https://api.coveragedetails.net/index.php/'
+    API_URL: 'https://m2-api-nest-test.herokuapp.com/'
   })
 
 ;
